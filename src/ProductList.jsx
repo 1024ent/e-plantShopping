da794a5b1a212c6b53238059f394b7a141459229
1024ent <shonenent1024@gmail.com>
2024-09-26 09:45:21 -0400

    Final code

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -10,13 +10,14 @@ function ProductList() {
     const [addedToCart, setAddedToCart] = useState({});
     const carItems = useSelector((state) => state.cart);
     const dispatch = useDispatch();
-    const cartItems = useSelector(state => state.cart.times);
-
+    const cartItems = useSelector(state => state.cart.items);
     console.log(cartItems);
+
     // setCart(cartItems);
     useEffect(() => {
         
     }, []);
+
     const alreadyInCart = (itemName) => {
         return cartItems.some((item) => item.name === itemName);
     }
@@ -280,7 +281,7 @@ function ProductList() {
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                         <a href="/" style={{textDecoration:'none'}}>
                             <div>
-                                <h3 style={{color:'white'}}>Paradise Nursery</h3>
+                                <h3 style={{color:'white'}}>Skibidi Sigma Nursery</h3>
                                 <i style={{color:'white'}}>Where Green Meets Serenity</i>
                             </div>
                         </a>
@@ -288,7 +289,7 @@ function ProductList() {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><label style={{zIndex:1,position:"fixed",fontSize:"1.5rem",cursor:"pointer"}}>{totalItems()}</label><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><label style={{zIndex:1,position:"fixed",fontSize:"1.5rem",cursor:"pointer"}}>{totalItems()}</label><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">0<rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart? (
@@ -301,9 +302,10 @@ function ProductList() {
                             <div className="product-card" key={plantIndex}>
                                 <img className="product-image" src={plant.image} alt={plant.name} />
                                 <div className="product-title">{plant.name}</div>
+                                <h2>{plant.name}</h2>
                                 <p>{plant.description}</p>
                                 <p>{plant.cost}</p>
-                                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                <button style={{backgroundColor:alreadyInCart(plant.name)?"gray":"green"}} disabled={alreadyInCart(plant.name)? true:false} onClick={()=>handleAddToCart({name:plant.name,cost:plant.cost,image:plant.image})} className='product-button'>Add to Cart</button>
                             </div>
                             ))}
                         </div>
